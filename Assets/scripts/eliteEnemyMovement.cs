252a5294d65dab9776cae47a25690d15b495ca61
Morgan Rogers <morgan.rog@yahoo.com>
2022-03-24 18:35:40 -0500

    added enemy prefabs, enemy scripts, enemy movement paths, and an enemy spawner game object and script

--- Assets/scripts/eliteEnemyMovement.cs
+++ Assets/scripts/eliteEnemyMovement.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class basicEnemyMovement : MonoBehaviour
+public class eliteEnemyMovement : MonoBehaviour
 {
     public Transform player;
     private float speed = 30.0f;
     private float diveLimitZAxis;
-    private float waitTime = 4.0f;
+    private float waitTime = 6.0f;
+    private Vector3 pointA;
+    private Vector3 pointB;
 
     public List<GameObject> movementPaths = new List<GameObject>();
     public Transform[] moveSpots;
@@ -18,10 +20,13 @@ public class basicEnemyMovement : MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Player Ship").GetComponent<Transform>();
+
         movementPaths.Add(GameObject.Find("Enemy Path 1"));
         movementPaths.Add(GameObject.Find("Enemy Path 2"));
 
         moveSpots = SetRandomPath();
+
+        // spawn point
         this.transform.position = moveSpots[1].transform.position;
 
         diveLimitZAxis = player.position.z + 2;
@@ -30,7 +35,7 @@ public class basicEnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(index < moveSpots.Length)
+        if (index < moveSpots.Length)
         {
             PathMovement();
         }
@@ -42,6 +47,7 @@ public class basicEnemyMovement : MonoBehaviour
             }
             else
             {
+                PingPongMovement();
                 waitTime -= Time.deltaTime;
             }
         }
@@ -59,17 +65,25 @@ public class basicEnemyMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, moveSpots[index].position, speed * Time.deltaTime);
         if (Vector3.Distance(transform.position, moveSpots[index].position) < 0.1f)
         {
-            
+
             index += 1;
-             
+
         }
+    }
+
+    void PingPongMovement()
+    {
+        pointA = moveSpots[(moveSpots.Length - 1)].transform.position;
+        pointB = moveSpots[(moveSpots.Length - 1)].transform.position + new Vector3(15, 0, 0);
+        float time = Mathf.PingPong(Time.time + speed, 1);
+        transform.position = Vector3.Lerp(pointB, pointA, time);
     }
 
     void Dive()
     {
-        if(transform.position.z < diveLimitZAxis)
+        if (transform.position.z < diveLimitZAxis)
         {
-            transform.Translate(new Vector3(0,0,-1) * speed * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, -1) * speed * Time.deltaTime);
         }
         else
         {
